0d874fa36872eff2bab72d662ca291c5c65b84e5
Pavel Aponik <aponikpavel@gmail.com>
2022-11-08 21:29:04 +0300

    refactor testOpenCalculatorPage

--- src/test/java/by/it_academy/taf_wc/MainTest.java
+++ src/test/java/by/it_academy/taf_wc/MainTest.java
@@ -3,18 +3,27 @@ package by.it_academy.taf_wc;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MainTest {
    @Test
-    public void openCalculatorPage() {
+    public void testOpenCalculatorPage() {
         // given
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         String URL = "https://svyatoslav.biz/testlab/wt/index.php";
         // when
         driver.get(URL);
+      String xPathPageTitle =  "//body/table/tbody/tr[1]/td[1]";
+       By byPageTitle = By.xpath(xPathPageTitle);
+       WebElement webElementPageTitle = driver.findElement(byPageTitle);
+       String actualPageTitle = webElementPageTitle.getText();
+       System.out.println(actualPageTitle);
+
 
 
     }
+
 }
