a4ab9cef844377374d0cfe9265760adbd6e378c1
Aliaksandr Viartseika <aliaksandr_viartseika@leverx.com>
2022-11-08 18:34:01 +0100

    add openCalculatorPage method

--- src/test/java/by/it_academy/taf_wc/MainTest.java
+++ src/test/java/by/it_academy/taf_wc/MainTest.java
@@ -1,4 +1,20 @@
 package by.it_academy.taf_wc;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MainTest {
+   @Test
+    public void openCalculatorPage() {
+        // given
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        String URL = "https://svyatoslav.biz/testlab/wt/index.php";
+        // when
+        driver.get(URL);
+
+
+    }
 }
