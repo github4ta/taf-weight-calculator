dbef922942e19cb2f87d05cb21363240c77c83bc
Viktor Marchuk <viktor.m77.77@mail.ru>
2022-11-08 21:52:45 +0300

    refactor add step2

--- src/main/java/by/it_academy/taf_wc/Main.java
+++ src/main/java/by/it_academy/taf_wc/Main.java
@@ -1,4 +1,7 @@
 package by.it_academy.taf_wc;
 
 public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
 }
--- src/test/java/by/it_academy/taf_wc/MainTest.java
+++ src/test/java/by/it_academy/taf_wc/MainTest.java
@@ -7,21 +7,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class MainTest {
-   @Test
-    public void testOpenCalculatorPage() {
+public class MainTest{
+    @Test
+    public void testOpenCalculatorPage(){
         // given
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         String URL = "https://svyatoslav.biz/testlab/wt/index.php";
         // when
         driver.get(URL);
-      String xPathPageTitle =  "//body/table/tbody/tr[1]/td[1]";
-       By byPageTitle = By.xpath(xPathPageTitle);
-       WebElement webElementPageTitle = driver.findElement(byPageTitle);
-       String actualPageTitle = webElementPageTitle.getText();
-       System.out.println(actualPageTitle);
+        String xPathPageTitle = "//body/table/tbody/tr[1]/td[1]";
+        By byPageTitle = By.xpath(xPathPageTitle);
+        WebElement webElementPageTitle = driver.findElement(byPageTitle);
+        String actualPageTitle = webElementPageTitle.getText();
+        System.out.println(actualPageTitle);
 
+        String xPathInputName = "//input[@name='name']";
+        By byInputName = By.xpath(xPathInputName);
+        WebElement webElementInputName = driver.findElement(byInputName);
+        String name = "Vasia";
+        webElementInputName.sendKeys(name);
 
 
     }
