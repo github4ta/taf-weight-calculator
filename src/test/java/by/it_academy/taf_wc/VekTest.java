7b6b830cfa0f06b9f40939904edfdfc9fae1c352
hanna_klimasheuskaya <hanna.klimasheuskaya@gmail.com>
2022-11-10 21:57:08 +0300

    refactor class VekTest

--- src/test/java/by/it_academy/taf_wc/VekTest.java
+++ src/test/java/by/it_academy/taf_wc/VekTest.java
@@ -3,35 +3,19 @@ package by.it_academy.taf_wc;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class VekTest{
+public class VekTest {
     @Test
-    public void testAllActionsPage(){
-        String expected = "Все акции";
-        String url = "https://www.21vek.by/";
-        String xPathAllActionsLink = "//a[@href='/special_offers/promo.html'][@data-index='0']";
-        String xPathAllActionsPageTitle = "//div[@class='style_title__fFdOX style_titleNew__2HvW8']";
-
+    public void testAllActionsPage() {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
+        VekPage vekPage = new VekPage(driver);
 
-        driver.navigate().to(url);
-        Util.waiter();
-        By byButtonAgreement = By.xpath("//button[@data-testid='agreementBtn']");
-        WebElement elementButtonAgreement = driver.findElement(byButtonAgreement);
-        elementButtonAgreement.click();
+        vekPage.openHomePageAngClickButtonAgreementAndClickAllActionsLink();
+        String actual = vekPage.getAllActionsPageTitleText();
 
-        By byAllActionsLink = By.xpath(xPathAllActionsLink);
-        WebElement elementAllActionsLink = driver.findElement(byAllActionsLink);
-        elementAllActionsLink.click();
-
-        By byAllActionsPageTitle = By.xpath(xPathAllActionsPageTitle);
-        WebElement elementAllActionsPageTitle = driver.findElement(byAllActionsPageTitle);
-        String actual = elementAllActionsPageTitle.getText();
-
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(vekPage.EXPECTED, actual);
+        driver.quit();
     }
 }
