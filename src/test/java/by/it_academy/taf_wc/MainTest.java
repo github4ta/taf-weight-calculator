e5b7491aea2cfb12ad0eba470dc90e9caab9b872
hanna_klimasheuskaya <hanna.klimasheuskaya@gmail.com>
2022-11-10 19:25:57 +0300

    refactor class MainTest

--- src/test/java/by/it_academy/taf_wc/MainTest.java
+++ src/test/java/by/it_academy/taf_wc/MainTest.java
@@ -22,9 +22,6 @@ public class MainTest {
         By byPageTitle = By.xpath(xPathPageTitle);
         WebElement webElementPageTitle = driver.findElement(byPageTitle);
         String actualPageTitle = webElementPageTitle.getText();
-        System.out.println(actualPageTitle);
-
-
 
         String xPathInputName = "//input[@name='name']";
         By byInputName = By.xpath(xPathInputName);
@@ -32,36 +29,33 @@ public class MainTest {
         String name = "Vasia";
         webElementInputName.sendKeys(name);
 
-
         By byInputHeight = By.name("height");
         WebElement webElementInputHeight = driver.findElement(byInputHeight);
         String height = "165";
         webElementInputHeight.sendKeys(height);
-
 
         By byInputWeight = By.name("weight");
         WebElement webElementInputWeight = driver.findElement(byInputWeight);
         String weight = "55";
         webElementInputWeight.sendKeys(weight);
 
-
         String xPathRadioButtonGenderFemale = "//input[@value='f']";
         By byRadioButtonGenderFemale = By.xpath(xPathRadioButtonGenderFemale);
         WebElement webElementRadioButtonGenderFemale = driver.findElement(byRadioButtonGenderFemale);
         webElementRadioButtonGenderFemale.click();
 
+        String xPathButtonSubmit = "//input[@type='submit']";
+        By byButtonSubmit = By.xpath(xPathButtonSubmit);
+        WebElement webElementButtonSubmit = driver.findElement(byButtonSubmit);
+        webElementButtonSubmit.click();
 
         String xPathResult = "//tbody/tr[2]/td[2]";
         By byResult = By.xpath(xPathResult);
         WebElement webElementResult = driver.findElement(byResult);
         String actual = webElementResult.getText();
-        System.out.println(actual);
+
         Assert.assertEquals(expected,actual);
 
-
         driver.quit();
-
-
     }
-
 }
